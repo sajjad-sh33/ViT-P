b021958b2ed653f12f4d1f55228d388496cb5098
sajjad-sh33 <79309701+sajjad-sh33@users.noreply.github.com>
2025-05-22 18:49:45 +0330

    Update train_ADE20K_base.sh

--- train_ADE20K_base.sh
+++ train_ADE20K_base.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH --nodes 1
-#SBATCH --mem=132G
 #SBATCH --gpus-per-node=a100:4
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=8
@@ -9,7 +8,6 @@
 
 cp -r dinov2 $SLURM_TMPDIR
 unzip -q ./backup/ADEChallengeData2016/ADEChallengeData2016.zip -d $SLURM_TMPDIR/ViT-P/datasets
-
 echo "*****Images are ready*****"
 
 
@@ -21,10 +19,8 @@ source ~/py311/bin/activate
 
 export PYTHONPATH=$PYTHONPATH:/home/projects/ViT-P
 
-
 cd $SLURM_TMPDIR
 cd ViT-P
-
 
 srun --unbuffered python dinov2/train/train.py --config-file dinov2/configs/train/vitb14_ADE20k.yaml --output-dir ./OUTPUT_DIR --no-resume
 
